5bb0682e45003057655f76349edd3708186e96d1
Vanshikasingh2008 <64592848+Vanshikasingh2008@users.noreply.github.com>
2021-06-14 17:24:27 +0530

    Update GuessingGame.py

--- Pro-97/GuessingGame.py
+++ Pro-97/GuessingGame.py
@@ -10,12 +10,8 @@ while chances>0 :
         chances = 0
     else:
         #if (number > randomNo):
-        if(chances <= 5 ):
+        if(chances <= 5):
             print("Wrong Answer.Try once again")
             chances-=1
-        elif (chances==0):
-            print("You Lost. Your all the chances have been used up. Sorry! Better luck next time")
-        #elif (number < randomNo):
-        #print("Wrong Answer. Try a number higher than yours")
-            #chances-=1
-
+            if (chances==0):
+                print("You Lost. Your all the chances have been used up. Sorry! Better luck next time")
