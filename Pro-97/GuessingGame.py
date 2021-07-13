f7791f79643bace20e09eadaf00b21d658261bf8
Vanshikasingh2008 <64592848+Vanshikasingh2008@users.noreply.github.com>
2021-07-13 19:29:34 +0530

    Update GuessingGame.py

--- Pro-97/GuessingGame.py
+++ Pro-97/GuessingGame.py
@@ -1,17 +1,18 @@
 import random 
 randomNo = random.randint(1,9)
-print(randomNo)
-chances = 5
+#print(randomNo)
+chances = 4
 print("Welcome to the Number Guessing game!")
-while chances>0 :
-    number = input("Enter a number between 1 and 9:- ")
+while chances<=4 :
+    number = int(input("Enter a number between 1 and 9:- "))
     if(number == randomNo):
         print("Congats!!! You won the Game.")
-        chances = 0
+        break
     else:
-        #if (number > randomNo):
-        if(chances <= 5):
+        if(chances <= 4 and chances>=1):
             print("Wrong Answer.Try once again")
-            chances-=1
-            if (chances==0):
-                print("You Lost. Your all the chances have been used up. Sorry! Better luck next time")
+        elif (chances<=0):
+            print("You Lost. Your all the chances have been used up. Sorry! Better luck next time")
+            print("Your actual no. was" , randomNo)
+            break
+        chances-=1
